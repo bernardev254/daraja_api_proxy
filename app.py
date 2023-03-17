43a36ebb0f79376dbfd173cdf2aecf8e20b20654
bkaraba <bkaraba14@gmail.com>
2023-03-17 16:34:23 +0300

    connection error

--- app.py
+++ app.py
@@ -16,16 +16,17 @@ def proxyAuth():
     url = config_by_name["prod"].URL
     token = config_by_name["prod"].TOKEN
     client_id = config_by_name["prod"].CLIENT_ID
-    client_id_from_request = request.args.get('client_id')
+    client_id_from_request = request.form.get('client_id')
     payload={}
     headers = {
         "Authorization": "Basic {}".format(token)
     }
-    if client_id == client_id_from_request:
+    same = (client_id == client_id_from_request)
+    if same:
         response = requests.get(url, headers=headers, data=payload)
         resp = response.json()
         return jsonify(resp), 200
-    return jsonify({"msg":"invalid credentials"}),401
+    return jsonify({"msg":"Unauthorized"}),401
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
