b8ba4f32168f12958ed9796d85a21ed701bf9348
bkaraba <bkaraba14@gmail.com>
2023-03-17 18:07:09 +0300

    refresh token

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app.config.from_object(config_by_name["prod"])
 
 @cross_origin(origins='*',methods=['POST','OPTIONS',])
 def proxyAuth():
-    #url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
     url = config_by_name["prod"].URL
     token = config_by_name["prod"].TOKEN
     client_id = config_by_name["prod"].CLIENT_ID
@@ -27,6 +26,29 @@ def proxyAuth():
         resp = response.json()
         return jsonify(resp), 200
     return jsonify({"msg":"Unauthorized"}),401
+
+
+@app.route('/refresh_token', methods=['POST'])
+def refresh_token():
+    # Extract the expired access token from the request body
+    expired_token = request.json.get('access_token')
+
+    # TODO: Use your authentication logic to obtain a new access token
+    url = config_by_name["prod"].URL
+    token = config_by_name["prod"].TOKEN
+    client_id = config_by_name["prod"].CLIENT_ID
+    client_id_from_request = request.form.get('client_id')
+    payload={}
+    headers = {
+        "Authorization": "Basic {}".format(token)
+    }
+    response = requests.get(url, headers=headers, data=payload)
+    resp = response.json()
+    new_token = resp
+
+    # Return the new access token in the response body
+    return jsonify({'access_token': new_token})
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
